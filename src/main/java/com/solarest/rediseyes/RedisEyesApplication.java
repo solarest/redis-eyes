c0ddca30c2e620d527a2bef2c4496df2bf7332c4
solarest <solarest@icloud.com>
2017-05-06 13:42:19 +0800

    init project

---
+++ src/main/java/com/solarest/rediseyes/RedisEyesApplication.java
@@ -0,0 +1,12 @@
+package com.solarest.rediseyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisEyesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisEyesApplication.class, args);
+	}
+}
